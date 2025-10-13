cc8fc3e6fc13115159b8323effc2451871d4c654
Samuel-5485 <samediriba54@gmail.com>
2025-10-13 19:54:06 +0300

    I get erro guesses is not defined so you have to check this later!

--- NumberGuessing/number_guess.py
+++ NumberGuessing/number_guess.py
@@ -6,19 +6,19 @@ top_of_range = input("Type a number: ")
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
-    if top_of_range <=0:
+    if top_of_range <= 0:
         print("Please type a number larger than 0 next time.")
         quit()
+else:
+    print("Please type a number next time.")
+    quit()
 
-    else:
-        print("Please type a number next time.")
-        quit()
-
+    
 random_number = random.randint(0, top_of_range)
-# random_number = random.randint(1, 15)
+print(f"[DEBUG] Random number is: {random_number}")
 
 while True:
-    # print('Tim')
+    guesses = guesses + 1
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
        user_guess = int(user_guess)
@@ -29,4 +29,11 @@ while True:
 
     if user_guess == random_number:
         print("You got it!")
- 
+        break
+    else:
+        if user_guess > random_number:
+            print("You were abouove the number!")
+        else:
+            print("You were below the number!")
+        
+print("You got it in", guesses, "guesses")
